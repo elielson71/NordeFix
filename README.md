3f34e5f8d9103f55527e3eb54a11561173b028a5
elielson71 <elielsonboavista@gmail.com>
2021-02-24 11:13:36 -0300

    melhorias

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone-Netflix
 Clone da pagina da famosa Netflix com apenas HMTL,CSS e JS
+
---
+++ teste.js
@@ -0,0 +1,6 @@
+let km = parseInt(500);
+let com = parseFloat(35);
+if (com > 0) {
+    retorno = km / com
+    console.log("%f3", (retorno.toFixed(2)))
+}
